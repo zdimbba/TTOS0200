ea836a474004f8cffb3c77fe1448f9cc34c272ae
salesa <salesa@ICT-421-WS-00>
2017-01-16 13:58:53 +0200

    Lab3 Tehtävä 1 Kiuas tehty

---
+++ OOdemot/Kiuas.cs
@@ -0,0 +1,26 @@
+﻿/* TTOS0200 olo-ohjelmointi 
+ * esimerkki Kiuas-luokka
+ */
+using System;
+
+namespace JAMK.IT
+{
+  public class Kiuas
+  {
+    public bool OnkoPäällä { get; set; }
+    public float Lämpötila { get; set; }
+    //kosteus voi olla välillä 0-100
+    float kosteus;
+    public float Kosteus {
+      get {return kosteus;}
+      set
+      {
+        kosteus = value;
+        if (kosteus < 0 || kosteus > 100)
+        {
+          kosteus = 0;
+        }
+      }
+    }
+  }
+}
---
+++ OOdemot/Program.cs
@@ -0,0 +1,31 @@
+﻿using JAMK.IT; //helpottaa meidän luokkien löytämästi
+using System;
+
+namespace OOdemot
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestaaKiuas();
+    }
+
+    //Lab3 Tehtävä 1: Kiuas-luokan testaus
+    static void TestaaKiuas()
+    {
+      //luodaan kiuas olio
+      Kiuas kiuas = new JAMK.IT.Kiuas();
+      //pistetään kiuas lämpenemään ja asetetaan lämpöä&kosteutta
+      kiuas.OnkoPäällä = true;
+      kiuas.Lämpötila = 90;
+      kiuas.Kosteus = 50;
+      //näytetään konsolilla
+      Console.WriteLine("Kiuas on päällä {0}", kiuas.OnkoPäällä);
+      Console.WriteLine("Kiukaan lämpötila {0}", kiuas.Lämpötila);
+      Console.WriteLine("Kiukaan kosteus {0}", kiuas.Kosteus);
+      //mitä tapahtuu jos kosteus yli rajojen
+      kiuas.Kosteus = 101;
+      Console.WriteLine("Kiukaan kosteus {0}", kiuas.Kosteus);
+    }
+  }
+}
