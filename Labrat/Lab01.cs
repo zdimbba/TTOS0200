074138d54400d173422b71ed9cc31f9b6e8c6445
Zdimbba <K9296@student.jamk.fi>
2017-09-06 10:57:57 +0300

    Added project Labrat for week assignments.

---
+++ Labrat/Lab01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab01
+    {
+        public static void Tehtava1 ()
+        {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana.
+        }
+    }
+}
---
+++ Labrat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Olio-ohjelmoinnin viikkotehtävät Labra 1.
+// Timo Kannus
+namespace Labrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab01.Tehtava1();
+        }
+    }
+}
