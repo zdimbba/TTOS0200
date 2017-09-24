6c06ba2a30a5529f93173bf71d404d30924d83a9
Timo <K9296@student.jamk.fi>
2017-09-24 14:00:08 +0300

    Week 4 task 2/3

---
+++ DynaHissi/Hissi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynaHissi
+{
+    class Hissi
+    {
+        //Properties
+        private readonly int MinFloor = 1;
+        private readonly int MaxFloor = 5;
+        private int floor;
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value >= MinFloor && value <= MaxFloor) floor = value;
+                else
+                {
+                    Console.WriteLine("There are only five floors, try again");
+                    floor = Floor;
+                }
+            }
+    }
+    }
+}
---
+++ DynaHissi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynaHissi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elevator = 0;
+
+            Hissi hissi = new Hissi();
+            hissi.Floor = 1;
+
+            while (elevator < 5)
+            {
+                Console.WriteLine("Choose a floor: ");
+                hissi.Floor = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Current floor is: " + hissi.Floor);
+                elevator++;
+            }
+        }
+    }
+}
