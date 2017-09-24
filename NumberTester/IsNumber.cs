60644fca0f20173fb5f044758978da353b99c3d9
Timo <K9296@student.jamk.fi>
2017-09-24 12:26:40 +0300

    Week 4 task 1/3.

---
+++ NumberTester/IsNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTester
+{
+    class IsNumber
+    {
+        public static bool IsInt ()
+        {
+            //Ask for input
+            Console.WriteLine("Give a number: ");
+            string userinput = Console.ReadLine();
+
+            int number;
+
+            //Check if input is an integer
+            if (int.TryParse(userinput, out number))
+            {
+                return true;
+            }
+           
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsDate()
+        {
+            //Ask for input
+            Console.WriteLine("Give a number: ");
+            string userinput = Console.ReadLine();
+
+            DateTime date;
+
+            //Check if input is a date
+            if (DateTime.TryParse(userinput, out date))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NumberTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Viikko 4, tehtävä 1.
+//Staattiset metodit
+//Timo Kannus
+namespace NumberTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isnumber = NumberTester.IsNumber.IsInt();
+            bool isdate = NumberTester.IsNumber.IsDate();
+
+            Console.WriteLine("is a number: " + isnumber);
+            Console.WriteLine("is a date: " + isdate);
+        }
+    }
+}
